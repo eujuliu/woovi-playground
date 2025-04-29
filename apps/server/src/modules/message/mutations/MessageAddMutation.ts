fb72c456b8568c7ca26e88aebb6b519839ac51fe
Julio Martins <juliomarttins5@gmail.com>
2025-04-29 12:05:25 -0300

    test: added test suites for `message`
    added tests for queries and mutations of the `message`

--- apps/server/src/modules/message/mutations/MessageAddMutation.ts
+++ apps/server/src/modules/message/mutations/MessageAddMutation.ts
@@ -1,41 +1,57 @@
-import { GraphQLString, GraphQLNonNull } from "graphql";
-import { mutationWithClientMutationId } from "graphql-relay";
+import { GraphQLString, GraphQLNonNull, GraphQLError } from 'graphql';
+import { mutationWithClientMutationId } from 'graphql-relay';
 
-import { redisPubSub } from "../../pubSub/redisPubSub";
-import { PUB_SUB_EVENTS } from "../../pubSub/pubSubEvents";
+import { redisPubSub } from '../../pubSub/redisPubSub';
+import { PUB_SUB_EVENTS } from '../../pubSub/pubSubEvents';
 
-import { Message } from "../MessageModel";
-import { messageField } from "../messageFields";
+import { Message } from '../MessageModel';
+import { messageField } from '../messageFields';
 
 export type MessageAddInput = {
-  content: string;
+	content: string;
 };
 
 const mutation = mutationWithClientMutationId({
-  name: "MessageAdd",
-  inputFields: {
-    content: {
-      type: new GraphQLNonNull(GraphQLString),
-    },
-  },
-  mutateAndGetPayload: async (args: MessageAddInput) => {
-    const message = await new Message({
-      content: args.content,
-    }).save();
+	name: 'MessageAdd',
+	inputFields: {
+		content: {
+			type: new GraphQLNonNull(GraphQLString),
+		},
+	},
+	mutateAndGetPayload: async (args: MessageAddInput) => {
+		try {
+			if (args.content.trim().length < 1) {
+				throw new GraphQLError("Can't create an empty message", {
+					extensions: { code: 'EMPTY_MESSAGE' },
+				});
+			}
 
-    redisPubSub.publish(PUB_SUB_EVENTS.MESSAGE.ADDED, {
-      message: message._id.toString(),
-    });
+			const message = await new Message({
+				content: args.content,
+			}).save();
 
-    return {
-      message: message._id.toString(),
-    };
-  },
-  outputFields: {
-    ...messageField("message"),
-  },
+			redisPubSub.publish(PUB_SUB_EVENTS.MESSAGE.ADDED, {
+				message: message._id.toString(),
+			});
+
+			return {
+				message: message._id.toString(),
+			};
+		} catch (error) {
+			if (error instanceof GraphQLError) {
+				throw error;
+			}
+
+			throw new GraphQLError('Internal server error', {
+				extensions: { code: 'INTERNAL_SERVER_ERROR', originalError: error },
+			});
+		}
+	},
+	outputFields: {
+		...messageField('message'),
+	},
 });
 
 export const MessageAddMutation = {
-  ...mutation,
+	...mutation,
 };
--- apps/web/src/components/subscriptions/useMessageAddedSubscription.ts
+++ apps/web/src/components/subscriptions/useMessageAddedSubscription.ts
@@ -1,26 +1,26 @@
-import { useMemo } from "react";
-import { useSubscription } from "react-relay";
-import { GraphQLSubscriptionConfig } from "relay-runtime";
+import { useMemo } from 'react';
+import { useSubscription } from 'react-relay';
+import { GraphQLSubscriptionConfig } from 'relay-runtime';
 import {
-  MessageAddedSubscription,
-  MessageAddedSubscription$variables,
-} from "../../__generated__/MessageAddedSubscription.graphql";
-import { MessageAdded } from "./MessageAddedSubscription";
+	MessageAddedSubscription,
+	MessageAddedSubscription$variables,
+} from '../../__generated__/MessageAddedSubscription.graphql';
+import { MessageAdded } from './MessageAddedSubscription';
 
 const useMessageAddedSubscription = (
-  variables: MessageAddedSubscription$variables,
+	variables: MessageAddedSubscription$variables
 ) => {
-  const newMessageConfig = useMemo<
-    GraphQLSubscriptionConfig<MessageAddedSubscription>
-  >(
-    () => ({
-      subscription: MessageAdded,
-      variables,
-    }),
-    [variables],
-  );
+	const newMessageConfig = useMemo<
+		GraphQLSubscriptionConfig<MessageAddedSubscription>
+	>(
+		() => ({
+			subscription: MessageAdded,
+			variables,
+		}),
+		[variables]
+	);
 
-  useSubscription(newMessageConfig);
+	useSubscription(newMessageConfig);
 };
 
 export { useMessageAddedSubscription };
